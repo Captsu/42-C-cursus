2688009b6a506db1f1dc389e0a8666cae66a0132
Captsu <captsuu@gmail.com>
2026-09-05 19:13:35 +0200

    Libft

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emougeno <emougeno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/05 17:41:11 by emougeno          #+#    #+#             */
+/*   Updated: 2026/09/05 18:37:33 by emougeno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	neg;
+	int	nb;
+
+	neg = 1;
+	i = 0;
+	nb = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nb * neg);
+}
---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emougeno <emougeno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/05 18:02:45 by emougeno          #+#    #+#             */
+/*   Updated: 2026/09/05 18:48:34 by emougeno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (ft_strlen(little) == 0)
+		return ((char *) big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && (i + j < len) && big[i + j] == little[j])
+			j++;
+		if (little[j] == '\0')
+			return((char *) big + i);
+		i++;
+	}
+	return (NULL);
+}
+
+// int	main(void)
+// {
+// 	const char	*largestring = "Foo Bar Baz";
+// 	const char	*smallstring = "Bar";
+// 	char		*ptr;
+
+// 	ptr = ft_strnstr(largestring, smallstring, 3);
+// 	if (ptr)
+// 		printf("%s\n", ptr);
+// 	else
+// 		printf("NULL\n");
+// 	return (0);
+// }
